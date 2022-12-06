1f0a68f012281f9a221f373dfbae4810bc340ca8
DeysonDiaz <ddiazti@unsa.edu.pe>
2022-12-06 14:38:25 -0500

    Login with Facebook

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+void main() => runApp(MaterialApp(
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? Key}) : super(key: Key);
+
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isLoggedIn = false;
+  Map _userObj = {};
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lab06"),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: _isLoggedIn == true
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(_userObj["name"]),
+                  Text(_userObj["email"]),
+                  TextButton(
+                      onPressed: () {
+                        FacebookAuth.instance.logOut().then((value) {
+                          setState(() {
+                            _isLoggedIn = false;
+                            _userObj = {};
+                          });
+                        });
+                      },
+                      child: Text("Logout"))
+                ],
+              )
+            : Center(
+                child: ElevatedButton(
+                    child: Text("Login with Facebook"),
+                    onPressed: () async {
+                      FacebookAuth.instance
+                          .login(permissions: ["public_profile", "email"]).then(
+                              (value) {
+                        FacebookAuth.instance
+                            .getUserData()
+                            .then((userData) async {
+                          setState(() {
+                            _isLoggedIn = true;
+                            _userObj = userData;
+                          });
+                        });
+                      });
+                    }),
+              ),
+      ),
+    );
+  }
+}
